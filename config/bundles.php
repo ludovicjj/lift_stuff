d8eaf35a2b9fc6e39817797e891181327b28c341
ludovicjj <jahanlud@gmail.com>
2022-06-10 16:31:18 +0200

    refactor fixtures with foundry

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
 ];
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,40 +3,27 @@
 namespace App\DataFixtures;
 
 use App\Entity\RepLog;
-use App\Entity\User;
+use App\Factory\RepLogFactory;
+use App\Factory\UserFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
-    public function __construct(private UserPasswordHasherInterface $passwordHasher)
-    {
-    }
-
     public function load(ObjectManager $manager): void
     {
-        $user = new User();
+        $user = UserFactory::createOne(['email' => 'user@example.com', 'password' => 'pass-42']);
         $items = array_flip(RepLog::getAllowedLiftItems());
 
-        // hashed password
-        $password = $this->passwordHasher->hashPassword($user, 'pass-42');
-
-        $user
-            ->setEmail('user@email.com')
-            ->setPassword($password);
-
-        $manager->persist($user);
-
-        for($i = 0; $i < rand(1, 3); $i++) {
-            $repLog = new RepLog();
-            $repLog
-                ->setUser($user)
-                ->setReps(rand(1, 30))
-                ->setItem(array_rand($items));
-            $manager->persist($repLog);
-        }
-
-        $manager->flush();
+        RepLogFactory::createMany(
+            3,
+            function () use ($user, $items) {
+                return [
+                    'reps' => rand(1, 30),
+                    'item' => array_rand($items),
+                    'user' => $user
+                ];
+            }
+        );
     }
 }
