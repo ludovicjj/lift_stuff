a822e338cc44cf1b4f44033268c6117af96acd58
ludovicjj <jahanlud@gmail.com>
2022-06-11 09:57:12 +0200

    test: login fail

--- tests/Functional/Security/LoginTest.php
+++ tests/Functional/Security/LoginTest.php
@@ -15,7 +15,7 @@ class LoginTest extends WebTestCase
 {
     use ResetDatabase, Factories;
 
-    #[NoReturn] public function testLogin(): void
+    public function testLogin(): void
     {
         $client = static::createClient();
 
@@ -37,5 +37,78 @@ class LoginTest extends WebTestCase
         $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);
         $client->followRedirect();
         $this->assertRouteSame("home");
+    }
+
+    public function testLoginWithWrongPassword(): void
+    {
+        $client = static::createClient();
+
+        UserFactory::createOne([
+            'email' => 'test@example.com',
+            'password' => 'pass-123'
+        ]);
+        /** @var UrlGeneratorInterface $urlGenerator */
+        $urlGenerator = $client->getContainer()->get("router");
+        $crawler = $client->request(Request::METHOD_GET, $urlGenerator->generate('login'));
+
+        $form = $crawler->filter('form')->form([
+            '_email' => "john@example.com",
+            '_password' => "fail",
+        ]);
+
+        $client->submit($form);
+        $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);
+        $client->followRedirect();
+        $this->assertRouteSame("login");
+        $this->assertSelectorTextContains("div.alert-error", "Invalid credentials.");
+    }
+
+    public function testLoginWithWrongEmail(): void
+    {
+        $client = static::createClient();
+
+        UserFactory::createOne([
+            'email' => 'test@example.com',
+            'password' => 'pass-123'
+        ]);
+        /** @var UrlGeneratorInterface $urlGenerator */
+        $urlGenerator = $client->getContainer()->get("router");
+        $crawler = $client->request(Request::METHOD_GET, $urlGenerator->generate('login'));
+
+        $form = $crawler->filter('form')->form([
+            '_email' => "fail@example.com",
+            '_password' => "pass-123",
+        ]);
+
+        $client->submit($form);
+        $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);
+        $client->followRedirect();
+        $this->assertRouteSame("login");
+        $this->assertSelectorTextContains("div.alert-error", "Invalid credentials.");
+    }
+
+    public function testLoginWithWrongCsrfToken(): void
+    {
+        $client = static::createClient();
+
+        UserFactory::createOne([
+            'email' => 'test@example.com',
+            'password' => 'pass-123'
+        ]);
+        /** @var UrlGeneratorInterface $urlGenerator */
+        $urlGenerator = $client->getContainer()->get("router");
+        $crawler = $client->request(Request::METHOD_GET, $urlGenerator->generate('login'));
+
+        $form = $crawler->filter('form')->form([
+            '_email' => "test@example.com",
+            '_password' => "pass-123",
+            "_csrf_token" => "wrong_csrf_token"
+        ]);
+
+        $client->submit($form);
+        $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);
+        $client->followRedirect();
+        $this->assertRouteSame("login");
+        $this->assertSelectorTextContains("div.alert-error", "Invalid CSRF token.");
     }
 }
