be0916b54440a62b9dda5a19107257c7ea67e768
ludovicjj <jahanlud@gmail.com>
2022-06-18 01:00:32 +0200

    Update RepLogController::deleteRepLog

--- src/Controller/RepLogController.php
+++ src/Controller/RepLogController.php
@@ -7,6 +7,8 @@ use App\Security\RepLogVoter;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route("/api", name: "rep_log_")]
@@ -17,12 +19,17 @@ class RepLogController extends AbstractController
     }
 
     #[Route("/reps/{id}", name: "delete", methods: ['DELETE'])]
-    public function deleteRepLog(RepLog $repLog): JsonResponse
+    public function deleteRepLog(RepLog $repLog, Request $request): JsonResponse
     {
-        $this->denyAccessUnlessGranted(RepLogVoter::DELETE, $repLog);
-        $this->entityManager->remove($repLog);
-        $this->entityManager->flush();
+        $this->denyAccessUnlessGranted('ROLE_USER');
 
-        return new JsonResponse(null, 204);
+        if($request->isXmlHttpRequest()) {
+            $this->denyAccessUnlessGranted(RepLogVoter::DELETE, $repLog, "You are not allow to delete this RepLog");
+            $this->entityManager->remove($repLog);
+            $this->entityManager->flush();
+            return new JsonResponse(null, 204);
+        }
+
+        return new JsonResponse(['message' => 'Request header X-Requested-With is missing'], Response::HTTP_BAD_REQUEST);
     }
 }
