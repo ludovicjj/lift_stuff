d4bcb2f7c02296fcafc31d30739115bacf87bab4
ludovicjj <jahanlud@gmail.com>
2022-06-17 18:21:24 +0200

    Remove RepLog and flush

--- src/Controller/RepLogController.php
+++ src/Controller/RepLogController.php
@@ -4,18 +4,24 @@ namespace App\Controller;
 
 use App\Entity\RepLog;
 use App\Security\RepLogVoter;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route("/api", name: "rep_log_")]
 class RepLogController extends AbstractController
 {
+    public function __construct(private EntityManagerInterface $entityManager)
+    {
+    }
+
     #[Route("/reps/{id}", name: "delete", methods: ['DELETE'])]
-    public function deleteRepLog(RepLog $repLog, Request $request)
+    public function deleteRepLog(RepLog $repLog): JsonResponse
     {
         $this->denyAccessUnlessGranted(RepLogVoter::DELETE, $repLog);
+        $this->entityManager->remove($repLog);
+        $this->entityManager->flush();
 
         return new JsonResponse(null, 204);
     }
