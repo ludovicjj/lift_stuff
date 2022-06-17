acd593b444e04aa3c4dd6156c521c50a916478f7
ludovicjj <jahanlud@gmail.com>
2022-06-17 18:26:16 +0200

    Send Ajax Request And Hide Row

--- public/js/home.js
+++ public/js/home.js
@@ -2,6 +2,58 @@ const linkInfo = document.querySelector('#item-info');
 const modalInfo = new bootstrap.Modal(document.getElementById('itemInfoModal'));
 const acc = document.querySelector(".accordion-btn");
 
+////////////
+// select table.js-rep-log-table
+const tableRep = document.querySelector('.js-rep-log-table');
+// select all delete link into table.js-rep-log-table
+const tableDeleteLinks = tableRep.querySelectorAll('.js-delete-rep-log');
+// select all row (tr) into table tbody
+const tableRow = tableRep.querySelectorAll('tbody tr');
+
+tableRow.forEach((row) => {
+    row.addEventListener('click', () => {
+        console.log('row clicked')
+    })
+});
+
+tableDeleteLinks.forEach(deleteLink => {
+    deleteLink.addEventListener('click', async (e) => {
+        // disabled button
+        e.currentTarget.classList.add('disabled');
+        e.currentTarget.setAttribute('aria-disabled', true);
+        const row = e.currentTarget.closest('tr');
+
+        // icon spinner
+        const icon = e.currentTarget.querySelector('.fa-ban');
+        icon.classList.toggle('fa-spin');
+
+        // Get url to delete clicked repLog
+        const deleteUrl = e.currentTarget.getAttribute('data-url');
+
+        // AJAX
+        try {
+            const init = {
+                method: "DELETE",
+                headers: {
+                    "X-Requested-With": "XMLHttpRequest"
+                }
+            }
+            const response = await fetch(deleteUrl, init);
+            if (response.ok) {
+                row.classList.add('hide');
+                setTimeout(() => {
+                    row.classList.add('out');
+                }, 500)
+            } else {
+                throw new Error(`Request failed status code ${response.status}`)
+            }
+        } catch (err) {
+            console.log(err.message)
+        }
+    })
+});
+////////////
+
 linkInfo.addEventListener('click', (e) => {
     e.preventDefault();
     modalInfo.show();
