82c9e943117bf2eb164caaa805d98c4d37851b16
ludovicjj <jahanlud@gmail.com>
2022-06-21 10:29:25 +0200

    Update HomeTest with PantherTestCase

--- tests/Functional/Home/HomeTest.php
+++ tests/Functional/Home/HomeTest.php
@@ -2,31 +2,40 @@
 
 namespace App\Tests\Functional\Home;
 
+use App\Entity\User;
 use App\Factory\UserFactory;
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Panther\PantherTestCase;
 use Zenstruck\Foundry\Test\Factories;
 use Zenstruck\Foundry\Test\ResetDatabase;
+use Symfony\Component\Panther\Client;
 
-class HomeTest extends WebTestCase
+class HomeTest extends PantherTestCase
 {
     use ResetDatabase, Factories;
 
     public function testHomePageWhileLoggedIn(): void
     {
-        $client = static::createClient();
-        $proxy = UserFactory::createOne([
-            'email' => 'john@example.com',
-            'password' => 'pass-123'
+       $client = static::createPantherClient();
+
+        $user = UserFactory::createOne([
+            'email' => 'test@example.com',
+            'password' => 'password'
         ]);
 
-        $user = $proxy->object();
-        $client->loginUser($user);
-
-        // user is now logged in, so you can test protected resources
-        $client->request(Request::METHOD_GET, "/");
-        $this->assertResponseIsSuccessful();
+        $this->loginPantherClient($client);
+        $client->request('GET', '/');
         $this->assertSelectorTextContains('h2', 'Your Lift History');
-        $this->assertSelectorTextContains('td[colspan=4]', "Let's start to lift something !");
+        $this->assertSelectorTextContains('td[colspan="4"]', "Let's start to lift something !");
+        //$client->takeScreenshot('./var/error-screenshots/home.png');
+    }
+
+    private function loginPantherClient(Client $client)
+    {
+        $crawler = $client->request('GET', '/login');
+        $form = $crawler->filter('form[name=login]')->form([
+            '_email' => 'test@example.com',
+            '_password' => "password"
+        ]);
+        $client->submit($form);
     }
 }
