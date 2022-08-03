f0c85a79ab63442c6f273c5a68c955aea31d97c5
ludovicjj <jahanlud@gmail.com>
2022-08-03 06:12:31 +0200

    update menu

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,22 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
-console.log('hey');
+
+const showMenu = document.querySelector('.brand');
+const navBar = document.querySelector('.navbar');
+
+if (window.sessionStorage?.menu && window.sessionStorage.menu === 'show') {
+    console.log('state menu is show')
+    navBar.style.transition = 'margin-left 0s'
+    navBar.classList.add('active');
+}
+
+showMenu.addEventListener('click', () => {
+    navBar.style.transition = null;
+    navBar.classList.toggle('active');
+    if (navBar.classList.contains('active')) {
+        window.sessionStorage.setItem('menu', 'show')
+    } else {
+        window.sessionStorage.removeItem('menu')
+    }
+
+})
