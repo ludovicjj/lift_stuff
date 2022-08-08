cfc6d2c4d8568f2224eca59ef9a7e62b51a1447a
ludovicjj <jahanlud@gmail.com>
2022-08-08 07:25:13 +0200

    sandbox javascript

---
+++ public/js/play.js
@@ -0,0 +1,24 @@
+class AGreatClass {
+    constructor(greatNumber) {
+        this.greatNumber = greatNumber;
+    }
+    returnGreatThings() {
+        return this.greatNumber;
+    }
+}
+
+class AnotherGreatClass extends AGreatClass {
+    constructor(greatNumber, greatWord) {
+        super(greatNumber);
+        this.greatWord = greatWord
+    }
+    returnGreatThings() {
+        let greatNumber = super.returnGreatThings();
+        return [greatNumber, this.greatWord];
+    }
+}
+
+const aGreatObject = new AnotherGreatClass(42, 'adventure');
+console.log(
+    aGreatObject.returnGreatThings()
+);
