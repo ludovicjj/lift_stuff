5750d71922715a276f2bf3dbb8cd0b46b2ef5cb0
ludovicjj <jahanlud@gmail.com>
2022-08-17 09:17:16 +0200

    webpack fontawesome

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,8 @@
 const bootstrap = require('bootstrap');
 require('bootstrap/dist/css/bootstrap.css');
 require('../css/main.css');
+require('@fortawesome/fontawesome-free/css/all.css');
+require('@fortawesome/fontawesome-free/js/all.js');
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
 [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
