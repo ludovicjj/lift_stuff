0a9fb9fb7c49c58bc2a01fb9d3605176a955e13f
ludovicjj <jahanlud@gmail.com>
2022-08-18 18:49:26 +0200

    Rebuild navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 const bootstrap = require('bootstrap');
 require('bootstrap/dist/css/bootstrap.css');
-require('../css/main.css');
 require('@fortawesome/fontawesome-free/css/all.css');
 require('@fortawesome/fontawesome-free/js/all.js');
+require('../css/main.css');
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
 [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,20 +1,38 @@
-const showMenu = document.querySelector('.brand');
-const navBar = document.querySelector('.navbar');
+// const showMenu = document.querySelector('.brand');
+// const navBar = document.querySelector('.navbar');
+//
+// if (window.sessionStorage?.menu && window.sessionStorage.menu === 'show') {
+//     console.log('state menu is show')
+//     navBar.style.transition = 'margin-left 0s'
+//     navBar.classList.add('active');
+// }
+//
+// if (showMenu) {
+//     showMenu.addEventListener('click', () => {
+//         navBar.style.transition = null;
+//         navBar.classList.toggle('active');
+//         if (navBar.classList.contains('active')) {
+//             window.sessionStorage.setItem('menu', 'show')
+//         } else {
+//             window.sessionStorage.removeItem('menu')
+//         }
+//     })
+// }
+const nav = document.querySelector('.navbar');
+const content = document.querySelector('.container-content');
+const li = document.querySelector('.navbar-toggle');
+const span = li.querySelector('span');
+li.addEventListener('click', function (e) {
+    e.preventDefault();
+    nav.classList.toggle('active');
 
-if (window.sessionStorage?.menu && window.sessionStorage.menu === 'show') {
-    console.log('state menu is show')
-    navBar.style.transition = 'margin-left 0s'
-    navBar.classList.add('active');
-}
-
-if (showMenu) {
-    showMenu.addEventListener('click', () => {
-        navBar.style.transition = null;
-        navBar.classList.toggle('active');
-        if (navBar.classList.contains('active')) {
-            window.sessionStorage.setItem('menu', 'show')
-        } else {
-            window.sessionStorage.removeItem('menu')
-        }
-    })
-}
+    if (content.classList.contains('open')) {
+        content.classList.remove('open')
+        content.classList.add('close');
+        span.innerHTML = '<i class="fa-solid fa-bars"></i>';
+    } else {
+        span.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+        content.classList.add('open')
+        content.classList.remove('close')
+    }
+})
