d9314deb278181fca712d80cffe1e0b03b09a975
ludovicjj <jahanlud@gmail.com>
2022-08-19 14:55:31 +0200

    Load css and scss into main.scss

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 const bootstrap = require('bootstrap');
-require('bootstrap/dist/css/bootstrap.css');
-require('@fortawesome/fontawesome-free/css/all.css');
+// require('bootstrap/dist/css/bootstrap.css');
 require('@fortawesome/fontawesome-free/js/all.js');
 require('../css/main.scss');
 
+// Enable tooltip everywhere
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
 [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
