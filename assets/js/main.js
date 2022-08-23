9064faf9248904a22fa2d600e24f81cbcac6c285
ludovicjj <jahanlud@gmail.com>
2022-08-23 11:28:40 +0200

    require menu.js into main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,7 @@ const bootstrap = require('bootstrap');
 // require('bootstrap/dist/css/bootstrap.css'); // Now load scss into main.scss
 require('@fortawesome/fontawesome-free/js/all.js');
 require('../css/main.scss');
+require('./menu.js')
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
 [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,26 +1,7 @@
-// const showMenu = document.querySelector('.brand');
-// const navBar = document.querySelector('.navbar');
-//
-// if (window.sessionStorage?.menu && window.sessionStorage.menu === 'show') {
-//     console.log('state menu is show')
-//     navBar.style.transition = 'margin-left 0s'
-//     navBar.classList.add('active');
-// }
-//
-// if (showMenu) {
-//     showMenu.addEventListener('click', () => {
-//         navBar.style.transition = null;
-//         navBar.classList.toggle('active');
-//         if (navBar.classList.contains('active')) {
-//             window.sessionStorage.setItem('menu', 'show')
-//         } else {
-//             window.sessionStorage.removeItem('menu')
-//         }
-//     })
-// }
 const nav = document.querySelector('.navbar');
 const content = document.querySelector('.container-content');
 const navToggleLink = document.querySelector('a.navbar-toggle-link');
+
 if (navToggleLink) {
     const span = navToggleLink.querySelector('span');
     navToggleLink.addEventListener('click', function (e) {
